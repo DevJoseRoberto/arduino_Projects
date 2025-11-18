802ea4c27821569a304f3dd7dfbd726eccf5ca20
Jose Roberto <jrobertofilho2@gmail.com>
2025-11-18 10:33:15 -0400

    Ajeitando a arquitetura do codigo

--- Projeto_01/projeto01.cpp
+++ Projeto_01/projeto01.cpp
@@ -1,4 +1,4 @@
-//Declaração de variaveis
+// Declaração de variaveis
 
 int Button1 = 4;
 int Button2 = 6;
@@ -7,28 +7,26 @@ int estado_Button1 = 0;
 int estado_Button2 = 0;
 int temp = 5000;
 
+// Programa principal
 
-// Programa principal 
-
-void setup(){
- pinMode(Button1,INPUT);
- pinMode(Button2,INPUT);
- pinMode(LED,OUTPUT);
+void setup()
+{
+   pinMode(Button1, INPUT);
+   pinMode(Button2, INPUT);
+   pinMode(LED, OUTPUT);
 }
 
-void loop(){
-  estado_Button1 = digitalRead(Button1);
-  estado_Button2 = digitalRead(Button2);
-  
-  
-  if((estado_Button1 == HIGH) && (estado_Button2 == HIGH)){
-     digitalWrite(LED, HIGH);
-    
-  }else{
-     digitalWrite(LED, LOW);        
-  }
-  
-    
-    
+void loop()
+{
+   estado_Button1 = digitalRead(Button1);
+   estado_Button2 = digitalRead(Button2);
 
+   if ((estado_Button1 == HIGH) && (estado_Button2 == HIGH))
+   {
+      digitalWrite(LED, HIGH);
+   }
+   else
+   {
+      digitalWrite(LED, LOW);
+   }
 }
